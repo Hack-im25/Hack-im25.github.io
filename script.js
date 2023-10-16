0a34d5bd9058cfaf32c4a17e2cc2c653bff87c8f
Hack-im25 <106903503+Hack-im25@users.noreply.github.com>
2023-10-16 15:36:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function openModal(id) {
 function closeModal(id) {
     document.getElementById(id).style.display = 'none';
 }
+function toggleMenu() {
+    var mobileNav = document.getElementById("mobile-nav");
+    if (mobileNav.style.display === "none" || mobileNav.style.display === "") {
+        mobileNav.style.display = "block";
+    } else {
+        mobileNav.style.display = "none";
+    }
+}
+
+// Hide menu when a link is clicked
+document.querySelectorAll("#mobile-nav nav ul li a").forEach(function(link) {
+    link.addEventListener("click", function() {
+        document.getElementById("mobile-nav").style.display = "none";
+    });
+});
+window.addEventListener("scroll", function() {
+    document.getElementById("mobile-nav").style.display = "none";
+});
